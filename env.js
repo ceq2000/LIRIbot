d58c60b62c69c337c1923d4cf3a03b1c467afe08
Carlos Quinones <53359356+ceq2000@users.noreply.github.com>
2019-10-22 16:30:05 -0400

    Delete env.js

--- env.js
+++
@@ -1,5 +0,0 @@
-// const config = { id:'e2901ff1f8c34f9e9531f8b3b379a434', secret:'ba75dea8b0b2455abde330880da4e8e2'}
-// export default config
-
-const config = { id: 'e2901ff1f8c34f9e9531f8b3b379a434', secret: 'ba75dea8b0b2455abde330880da4e8e2' }
-module.exports = config
